cb427eac7e0da26cf0a2f8c3d9b1feacc798a1c8
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-09-26 14:44:18 +0200

    feat: exercise 4 done

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,31 +1,35 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
 /**
  * Funzione howManyVowels
- * 
+ *
  * @param {string} word
  * @returns {number}
  */
 
 function howManyVowels(inputWord) {
-    let numberOfVowels = 0;
-    for (let i = 0; i < inputWord.length; i++) {
-        const curLetter = inputWord[i].toLowerCase();
-        if (curLetter === "a" || curLetter === "e" || curLetter === "i" || curLetter === "o" || curLetter === "u") {
-            numberOfVowels++;
-        }
+  let numberOfVowels = 0;
+  for (let i = 0; i < inputWord.length; i++) {
+    const curLetter = inputWord[i].toLowerCase();
+    if (
+      curLetter === "a" ||
+      curLetter === "e" ||
+      curLetter === "i" ||
+      curLetter === "o" ||
+      curLetter === "u"
+    ) {
+      numberOfVowels++;
     }
-    return numberOfVowels;
+  }
+  return numberOfVowels;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
 console.log(howManyVowels(word));
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
 
+/**
+ * Funzione anonima
+ *
+ * @param {string} word
+ * @returns {string}
+ */
+
+const sayHello = (word) => "ciao " + word;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(sayHello(name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
