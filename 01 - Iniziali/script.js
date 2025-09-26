20cb2906e9f546ee0bdb70ee3dcb325fc6ed01e6
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-09-26 13:53:56 +0200

    feat: exercise 1 done

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,27 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+/**
+ * Funzione firstLetterOfArray
+ *
+ * @param {array}
+ * @returns {array}
+ */
+
+function firstLetterOfArray(array) {
+  const lettersArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const curName = array[i];
+    lettersArray[i] = curName[0];
+  }
+  return lettersArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(firstLetterOfArray(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
